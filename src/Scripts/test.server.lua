565f9a6e530f0a447224984ddf7b8ba2dbc71cd1
CluelessD3v <cluelessdevmail@gmail.com>
2022-10-14 06:06:04 -0500

    Initial commit

---
+++ src/Scripts/test.server.lua
@@ -0,0 +1,15 @@
+local RS         = game:GetService("ReplicatedStorage")
+local cs = game:GetService("CollectionService")
+local FindTagged = require(RS["FindTagged"])
+
+local Model = workspace.Model
+local parts = Model:GetChildren()
+local taggedPart= parts[math.ceil(#parts/2)]
+
+taggedPart.Name = "tagged"
+cs:AddTag(taggedPart, "test")
+
+
+
+
+
